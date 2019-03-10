2d519de573470838ecf16c38edc9c11eaf8e3316
Ayush Agarwal <thisisayushaa@gmail.com>
2019-03-10 22:04:13 +0530

    (update) UI Update 2

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,34 @@
-$(document).ready(function(e){
+$(document).ready(function (e) {
     var menu = $("#mainNavbar");
 
     // Initialise Particles
     particlesJS.load('homeParticles', 'static/js/particlesjs-config.json');
 
     // Scroll Operations
-    $(document).on('scroll', function(e){
+    $(document).on('scroll', function (e) {
         checkMenu();
     });
 
-    function init(){
+    function init() {
         checkMenu();
+        hidePreloader();
     }
 
     // Menu Checker
-    function checkMenu(){
+    function checkMenu() {
         let scrollH = $(document).scrollTop();
-        if(scrollH > 0){
+        if (scrollH > 0) {
             menu.addClass('fixed-layout');
-        }else{
+        } else {
             menu.removeClass('fixed-layout');
         }
+    }
+
+    function hidePreloader() {
+        $("#preloader").find('.absolute-center').fadeOut();
+        setTimeout(function () {
+            $("#preloader").fadeOut();
+        }, 500);
     }
 
     init();
